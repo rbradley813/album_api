d4bbde4af884c3c25dbacfa35b0e6f5c7f8d2bd0
rbradley813 <rbradley813@gmail.com>
2025-02-24 20:15:38 -0500

    initialize tables

--- entrypoint.py
+++ entrypoint.py
@@ -1,9 +1,19 @@
 from app import db, startup_app
 from app.api.albums.models import Album
+from app.api.artists.models import Artist
 
 app = startup_app()
 
 with app.app_context():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+    init_artist = Artist("init")
+    db.session.add(init_artist)
+    db.session.commit()
+    init_album = Album("init")
+    db.session.add(init_album)
+    db.session.commit()
     db.drop_all()
     db.create_all()
     db.session.commit()
