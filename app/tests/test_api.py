d8dca09a09ff3f8dd61bc1847abc4fea8349967d
rbradley813 <rbradley813@gmail.com>
2025-02-24 21:20:17 -0500

    add more tests

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -5,3 +5,23 @@ def test_ping(with_app):
     data = json.loads(resp.data.decode())
     assert resp.status_code == 200
     assert data["message"] == "pong!"
+
+def test_artists(with_app):
+    resp = with_app.test_client().get("/api/v1/artists")
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+
+def test_create_artist(with_app):
+    resp = with_app.test_client().get("/api/v1/artists?artists?name=test")
+    print(resp.data)
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert data["name"] == "test_artist"
+
+def test_create_album(with_app):
+    resp = with_app.test_client().get("/api/v1/albums?artist_name=test_artist&name=test_album&release_date=20050201&price=12.34")
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert data["name"] == "test_album"
+    assert data["release_date"] == "20050201"
+    assert data["price"] == 12.34
